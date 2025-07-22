36cb84353ec4c2e4df4afb0e868c00a6d0e3cf6d
Şükrü Yasin Yıldırım <144279767+yasinyildiriw@users.noreply.github.com>
2025-07-22 15:53:29 +0300

    Add files via upload

---
+++ AngelaYuDay37/main.py
@@ -0,0 +1,48 @@
+import requests
+from datetime import datetime
+
+today = datetime.now()
+
+USERNAME = "yasinq"
+TOKEN = "kajshfkbaewkuahcn238w9asd"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_parameters = {
+    "token" : TOKEN,
+    "username" : USERNAME,
+    "agreeTermsOfService" : "yes",
+    "notMinor" : "yes",
+}
+#
+# response = requests.post(url=pixela_endpoint,json=user_parameters)
+# print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id" : "graph1",
+    "name" : "Running Graph",
+    "unit" : "Km",
+    "type" : "float",
+    "color" : "sora"
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+
+# response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+
+quantity = {
+    "date" : today.strftime("%Y%m%d"),
+    "quantity" : input("How many kilometers did you run today? "),
+}
+
+response = requests.post(url=f"{graph_endpoint}/graph1",json=quantity,headers=headers)
+print(response.text)
+
+# response = requests.put(url=f"{graph_endpoint}/graph1/{today.strftime("%Y%m%d")}",json=quantity,headers=headers)
+# print(response.text)
+
+# response = requests.delete(url=f"{graph_endpoint}/graph1/{today.strftime("%Y%m%d")}", headers=headers)
+# print(response.text)
